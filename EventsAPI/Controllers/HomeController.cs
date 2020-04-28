1389e452c2029ef5ed89629368cbcf8fe24c9d00
apowel <austingpowel@gmail.com>
2020-04-28 10:00:33 -0400

    Implemented ticketmaster.discovery nuget package and added two actions to test functionality

--- EventsAPI/Controllers/HomeController.cs
+++ EventsAPI/Controllers/HomeController.cs
@@ -6,21 +6,51 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EventsAPI.Models;
+using System.Net.Http;
+using Ticketmaster.Discovery.V2;
+using Ticketmaster.Core;
+using Ticketmaster.Discovery;
+using Ticketmaster.Discovery.V2.Models;
 
 namespace EventsAPI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Config _config;
+        private readonly DiscoveryApi _discovery;
+        //nuget package documentation for DiscoveryAPI https://github.com/SerhiiVoznyi/Ticketmaster-SDK/tree/master/src/Ticketmaster.Discovery
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _config = new Config("2kVlEu5eTcizQZ73bkzcleUGRaFcJhxp");
+            var factory = new ClientFactory();
+            _discovery = factory.Create<DiscoveryApi>(_config);
         }
 
         public IActionResult Index()
         {
-            return View();
+            var states = new States();
+            return View(states);
+        }
+        public async Task<IActionResult> Events(string city)
+        {
+            var request = new SearchEventsRequest();
+            request.AddQueryParameter(SearchEventsQueryParameters.city, city);
+            var response = await _discovery.Events.SearchEventsAsync(request);
+            EventsResponse result = new EventsResponse();
+            result.Events = response._embedded.Events;
+            return View(result);
+        }
+        public async Task<IActionResult> Venues(string statecode)
+        {
+            var request = new SearchVenuesRequest();
+            request.AddQueryParameter(SearchVenuesQueryParameters.stateCode, statecode);
+            var response = await _discovery.Venues.SearchVenuesAsync(request);
+            VenuesResponse result = new VenuesResponse();
+            result.Venues = response._embedded.Venues;
+            return View(result);
         }
 
         public IActionResult Privacy()
